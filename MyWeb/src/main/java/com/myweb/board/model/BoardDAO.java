26f57acbd483af4f05d3493bf749ec84cab358ce
kownjinhyun <0311wlsgus@gmail.com>
2023-05-09 18:08:04 +0900

    0509 JSP

--- MyWeb/src/main/java/com/myweb/board/model/BoardDAO.java
+++ MyWeb/src/main/java/com/myweb/board/model/BoardDAO.java
@@ -33,21 +33,20 @@ public class BoardDAO implements IBoardDAO {
 		return dao;
 	}
 	
-	//////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////
+	
 
 	@Override
 	public void regist(String writer, String title, String content) {
-		String sql = "INSERT INTO my_board (board_id, writer, title, content) "
-				+ "VALUES(board_seq.nextval, ?, ?, ?)";
-		
+		String sql = "INSERT INTO my_board "
+				+ "(board_id, writer, title, content) "
+				+ "VALUES(board_seq.NEXTVAL,?,?,?)";
 		try(Connection conn = ds.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setString(1, writer);
 			pstmt.setString(2, title);
 			pstmt.setString(3, content);
-			
 			pstmt.executeUpdate();
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -56,7 +55,7 @@ public class BoardDAO implements IBoardDAO {
 	@Override
 	public List<BoardVO> listBoard() {
 		List<BoardVO> articles = new ArrayList<>();
-		String sql = "SELECT * FROM my_board";
+		String sql = "SELECT * FROM my_board ORDER BY board_id DESC";
 		try(Connection conn = ds.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql);
 				ResultSet rs = pstmt.executeQuery()) {
@@ -71,7 +70,7 @@ public class BoardDAO implements IBoardDAO {
 						);
 				articles.add(vo);
 			}
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return articles;
@@ -79,17 +78,98 @@ public class BoardDAO implements IBoardDAO {
 
 	@Override
 	public BoardVO contentBoard(int bId) {
-		return null;
+		BoardVO vo = null;
+		String sql = "SELECT * FROM my_board WHERE board_id=" + bId;
+		try(Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()) {
+			if(rs.next()) {
+				vo = new BoardVO(
+							rs.getInt("board_id"),
+							rs.getString("writer"),
+							rs.getString("title"),
+							rs.getString("content"),
+							rs.getTimestamp("reg_date").toLocalDateTime(),
+							rs.getInt("hit")
+						);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return vo;
 	}
 
 	@Override
 	public void updateBoard(String title, String content, int bId) {
-
+		String sql = "UPDATE my_board "
+				+ "SET title=?, content=? "
+				+ "WHERE board_id=?";
+		try(Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, title);
+			pstmt.setString(2, content);
+			pstmt.setInt(3, bId);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
 	public void deleteBoard(int bId) {
+		String sql = "DELETE FROM my_board WHERE board_id=?";
+		try(Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, bId);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public List<BoardVO> searchBoard(String keyword, String category) {
+		List<BoardVO> searchList = new ArrayList<>();
+		String sql = "SELECT * FROM my_board "
+				+ "WHERE " + category + " LIKE ?";
+		try(Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, "%" +keyword + "%");
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				BoardVO vo = new BoardVO(
+							rs.getInt("board_id"),
+							rs.getString("writer"),
+							rs.getString("title"),
+							rs.getString("content"),
+							rs.getTimestamp("reg_date").toLocalDateTime(),
+							rs.getInt("hit")
+						);
+				searchList.add(vo);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return searchList;
+	}
+	
+	@Override
+	public void upHit(int bId) {
 
+		String sql = "UPDATE my_board SET hit=hit+1 "
+				+ "WHERE board_id=?";
+		try(Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, bId);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		
 	}
 
 }
---
+++ MyWeb/src/main/java/com/myweb/board/service/UpdateService.java
@@ -0,0 +1,26 @@
+package com.myweb.board.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.myweb.board.model.BoardDAO;
+import com.myweb.board.model.BoardVO;
+
+public class UpdateService implements IBoardService {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		
+		
+		BoardDAO.getInstance().updateBoard(
+				request.getParameter("bTitle"),
+				request.getParameter("bContent"),
+				Integer.parseInt(request.getParameter("bId")));
+		
+		
+		
+		
+	}
+
+}
